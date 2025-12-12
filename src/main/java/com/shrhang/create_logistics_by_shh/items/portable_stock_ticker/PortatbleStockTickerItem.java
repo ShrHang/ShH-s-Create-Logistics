667b3ed5f6ef79db94bd49ebe5f7f6475a0b2685
ShrHang <shhshrhang@gmail.com>
2025-12-12 15:22:29 +0800

    也许是第一个正式版

--- src/main/java/com/shrhang/create_logistics_by_shh/items/portable_stock_ticker/PortatbleStockTickerItem.java
+++ src/main/java/com/shrhang/create_logistics_by_shh/items/portable_stock_ticker/PortatbleStockTickerItem.java
@@ -7,84 +7,95 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
 import com.simibubi.create.content.logistics.stockTicker.StockTickerBlockEntity;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class PortatbleStockTickerItem extends Item {
     public PortatbleStockTickerItem(Properties props) {
         super(props);
     }
 
-    @Override
-    public InteractionResult useOn(UseOnContext context) {
-        Level level = context.getLevel();
-        Player player = context.getPlayer();
-        ItemStack stack = context.getItemInHand();
-
-        if (level.isClientSide() || player == null)
-            return InteractionResult.PASS;
-
-        if (player.isShiftKeyDown())
-            return linkTo(stack, level, context.getClickedPos());
-
-        return tryToOpenMenu(level, player, stack);
-    }
-
 //    @Override
-//    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-//        ItemStack stack = player.getItemInHand(hand);
+//    public InteractionResult useOn(UseOnContext context) {
+//        Level level = context.getLevel();
+//        Player player = context.getPlayer();
+//        ItemStack stack = context.getItemInHand();
 //
-//        if (level.isClientSide())
-//            return InteractionResultHolder.pass(stack);
+//        if (level.isClientSide() || player == null)
+//            return InteractionResult.PASS;
 //
-//        if (!player.isShiftKeyDown()) {
-//            InteractionResult result = tryToOpenMenu(level, player, stack);
-//            return new InteractionResultHolder<>(result, stack);
-//        }
-//        return InteractionResultHolder.pass(stack);
+//        if (player.isShiftKeyDown())
+//            return linkTo(stack, level, context.getClickedPos()) ? InteractionResult.SUCCESS : InteractionResult.PASS;
+//
+//        return tryToOpenMenu(level, player, stack);
 //    }
 
-    public static InteractionResult linkTo(ItemStack stack, Level level, BlockPos pos) {
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+
+        ItemStack stack = player.getItemInHand(hand);
+
+        if (level.isClientSide())
+            return InteractionResultHolder.pass(stack);
+
+        if (player.isShiftKeyDown()) {
+            BlockHitResult blockHitResult = (BlockHitResult) player.pick(player.getAttributeValue(Attributes.BLOCK_INTERACTION_RANGE), 0.0f, false);
+            BlockPos blockPos = blockHitResult.getBlockPos();
+            InteractionResult result = InteractionResult.PASS;
+            if (linkTo(stack, level, blockPos)) {
+                player.sendSystemMessage(Component.literal("已连接"));
+                result = InteractionResult.SUCCESS;
+            }
+            return new InteractionResultHolder<>(result, stack);
+        }
+
+        InteractionResult result = tryToOpenMenu(level, player, stack) ? InteractionResult.SUCCESS : InteractionResult.PASS;
+        return new InteractionResultHolder<>(result, stack);
+
+    }
+
+    public static boolean linkTo(ItemStack stack, Level level, BlockPos pos) {
         BlockEntity be = level.getBlockEntity(pos);
         if (be instanceof StockTickerBlockEntity) {
             LvPosRecord newRecord = new LvPosRecord(level.dimension(), pos);
             stack.set(ComponentRegister.LV_POS, newRecord);
-            return InteractionResult.SUCCESS;
+            return true;
         }
-        return InteractionResult.PASS;
+        return false;
     }
 
-    public static InteractionResult tryToOpenMenu(Level level, Player player, ItemStack stack) {
+    public static boolean tryToOpenMenu(Level level, Player player, ItemStack stack) {
         LvPosRecord record = stack.get(ComponentRegister.LV_POS);
         if (record == LvPosRecord.EMPTY || record == null) {
             player.sendSystemMessage(Component.literal("物品未链接"));
-            return InteractionResult.PASS;
+            return false;
         }
 
-        ResourceKey<Level> dimension = record.dimension();
+        ResourceKey<Level> targetDimension = record.dimension();
         BlockPos pos = record.pos();
 
-        Level targetLevel = player.level();
-        if (targetLevel.dimension() != dimension) {
+//        Level targetLevel = player.level();
+        if (level.dimension() != targetDimension) {
             player.sendSystemMessage(Component.literal("必须在链接所在的维度才能打开"));
-            return InteractionResult.FAIL;
+            return false;
         }
 
-        BlockEntity be = targetLevel.getBlockEntity(pos);
+        BlockEntity be = level.getBlockEntity(pos);
 
-        if (targetLevel.getBlockEntity(pos) instanceof StockTickerBlockEntity) {
+        if (level.getBlockEntity(pos) instanceof StockTickerBlockEntity) {
             player.openMenu(new RemoteStockKeeperRequestMenuProvider((StockTickerBlockEntity) be), buf ->
                     buf.writeBoolean(true).writeBoolean(false).writeBlockPos(pos));
-            return InteractionResult.SUCCESS;
+            return true;
         }
         player.sendSystemMessage(Component.literal("在链接位置未找到方块"));
 
-        return InteractionResult.PASS;
+        return false;
     }
 }
